c35edb3209479220a7c2452ec336568590a7426a
kungyao <masa10269@gmail.com>
2017-02-09 12:39:51 +0800

    add IcoSphere

---
+++ planetEditor/Assets/Creat_code/CreatIcoSphere.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CreateIcoSphere : MonoBehaviour
+{
+    static Camera cam;
+    static Camera lastUsedCam;
+
+    [MenuItem("Creat_Planet/IcoSphere")]
+    static void CreateWizard()
+    {
+        cam = Camera.current;
+        // Hack because camera.current doesn't return editor camera if scene view doesn't have focus
+        if (!cam)
+            cam = lastUsedCam;
+        else
+            lastUsedCam = cam;
+
+        GameObject sphere = new GameObject();
+        sphere.AddComponent<IcoSphere>();
+        sphere.name = "IcoSphere";
+        sphere.transform.position = cam.transform.position + cam.transform.forward * 5.0f;
+
+    }
+}
---
+++ planetEditor/Assets/Creat_code/Creat_sphere.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Creat_sphere : MonoBehaviour
+{    
+    [MenuItem("Creat_Planet/Planet")]
+    static void Creat_Planet()
+    {
+        GameObject planet = new GameObject();
+        planet.AddComponent<Sphere>();
+        //planet.tag = "sun"; 
+        planet.name = "planet";
+    }
+}
